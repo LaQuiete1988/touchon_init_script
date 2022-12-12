eaf3fe965280b7b819c7c428839cc1d9956a23d5
Vitaliy Nikolaev <vn@touchon.tech>
2022-12-12 15:26:41 +0300

    sudo commands

--- init.sh
+++ init.sh
@@ -46,7 +46,7 @@ function docker_installation(){
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update && sudo apt-get install docker-ce -y
-    usermod -aG docker $USER
+    sudo usermod -aG docker $USER
     add_startup_script
     reboot
   fi
