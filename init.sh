629b5719645bb2acce132dedd01c79acabbd9ad0
Vitaliy Nikolaev <vn@touchon.tech>
2022-12-13 11:56:38 +0300

    release var added

--- init.sh
+++ init.sh
@@ -40,10 +40,11 @@ function docker_installation(){
   else
     sudo apt-get update && sudo apt-get install ca-certificates curl gnupg lsb-release -y
     sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    curl -fsSL https://download.docker.com/linux/$(lsb_release -is)/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(lsb_release -is) \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update && sudo apt-get install docker-ce -y
+    sudo groupadd docker
     sudo usermod -aG docker $USER
     add_startup_script
     reboot
